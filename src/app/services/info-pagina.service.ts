5f1c818c0f15fc0ffde9e426f6646669b7a01565
Marcos Susanibar <marcosup92@gmail.com>
2020-04-06 19:42:10 -0500

    Fin Seccion 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,19 +9,32 @@ export class InfoPaginaService {
 
   info: InfoPagina={};
   cargada=false;
+
+  equipo: any[]=[];
   
   constructor(private http:HttpClient) { 
 
     //console.log('Servicio de info Pagina Listo');
-
-    this.http.get('assets/data/data-pagina.json')
-    .subscribe((resp: InfoPagina)=>{
-
-      this.cargada=true;
-      this.info=resp;
-      console.log(resp);
-    });
+    this.cargarInfo();
+   this.cargarEquipo();
 
 
   }
+
+  private cargarInfo() {
+
+        this.http.get('assets/data/data-pagina.json')
+    .subscribe((resp: InfoPagina)=>{
+      this.cargada=true;
+      this.info=resp;
+    });
+  }
+
+  private cargarEquipo() {
+
+    this.http.get('https://angular-html-ac12d.firebaseio.com/equipo.json')
+.subscribe((resp: any[])=>{
+  this.equipo=resp;
+});
+}
 }
